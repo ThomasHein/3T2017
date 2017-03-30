f8a4689d1826f7fd917a0d6d85fc50ffd691b428
Christin Scheewe <scheewe@inmediasp.de>
2017-03-30 14:27:53 +0200

    Abhängigkeiten mit hinzugefügt

--- Backend/Mocks/StartMocks.bat
+++ Backend/Mocks/StartMocks.bat
@@ -1,4 +1,4 @@
 call npm install -g json-mock
 call npm install -g json-server
 start json-server -p 4000 Kundenverwaltung.json
-start json-server -p 3000 HelpService.json
+start json-server -p 5000 HelpService.json
