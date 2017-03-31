954697baf1eb2261f3ce016b000771c9b6bcb08d
Ferris <burock@inmediasp.de>
2017-03-31 09:01:41 +0200

    PP und Tests

---
+++ ReactTest/hello-world/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+
+/*class App extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;*/
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
